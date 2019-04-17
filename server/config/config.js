072cba78ebd7a74cbfc39668435e633401058b0e
wangcaowei <414162938@qq.com>
2019-04-17 14:51:12 +0800

    local config

---
+++ server/config/config.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: wangcaowei
+ * @Date: 2018-12-29 15:25:25
+ * @Last Modified by: wangcaowei
+ * @Last Modified time: 2019-04-17 14:48:41
+ */
+
+exports.databaseConf= {
+    host : "localhost",
+    user : "root",
+    password : "cvosolli123",
+    database : "koatest"
+}
+
+exports.tokenConf= {
+    secret : "ouermaimengBlog", //token私钥
+    userValidDate : "2d",
+    verifyValiDate : "1m"
+}
---
+++ server/config/config.ts
@@ -0,0 +1,19 @@
+/*
+ * @Author: wangcaowei
+ * @Date: 2018-12-29 15:25:25
+ * @Last Modified by:   wangcaowei
+ * @Last Modified time: 2018-12-29 15:25:25
+ */
+
+export enum databaseConf {
+    host = "localhost",
+    user = "root",
+    password = "cvosolli123",
+    database = "koatest"
+}
+
+export enum tokenConf {
+    secret = "ouermaimengBlog", //token私钥
+    userValidDate = "2d",
+    verifyValiDate = "1m"
+}
