740ff02ef20311dfe0f219b906fa104d9e96007c
wangcaowei <414162938@qq.com>
2019-04-19 15:18:00 +0800

    sequelize.import 去掉文件后缀

--- server/tool/utils.ts
+++ server/tool/utils.ts
@@ -2,15 +2,13 @@
  * @Author: wangcaowei
  * @Date: 2019-01-07 17:16:13
  * @Last Modified by: wangcaowei
- * @Last Modified time: 2019-02-25 19:38:58
+ * @Last Modified time: 2019-04-19 15:17:25
  */
 import * as path from "path";
 import * as bcrypt from "bcrypt";
 import * as dbDef from "../models/db";
 import sequelize from "../db/db";
-const user: dbDef.usersModel = sequelize.import(
-    path.join(__dirname, "../models/users.ts")
-);
+const user: dbDef.usersModel = sequelize.import("../models/users");
 
 /**
  * @description 验证用户名密码是否匹配
